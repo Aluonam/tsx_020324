bca3538e61974c0f29c6c6424a89240ea95650c7
Paula Cano <paulacanoroman@gmail.com>
2024-03-02 13:38:20 +0100

    feat: useState and type

--- components/tableData/TableData.tsx
+++ components/tableData/TableData.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TableData = () => {
+
+    type dataStorePrototype = {
+        id: number,
+        title: string,
+        price: number,
+        description: string,
+        category: string,
+        image: string,
+        rating: {
+            rate: number,
+            count: number
+            }
+    }
+
+    const [dataStore, setDataStore] = useState<dataStorePrototype>()
   return (
     <div>TableData</div>
   )
