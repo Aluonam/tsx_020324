27d7d9e4f6b0562ddaf61ebd1122e21304b5f5ad
Paula Cano <paulacanoroman@gmail.com>
2024-03-02 13:22:03 +0100

    feat: create and import component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import TableData from "@/components/tableData/TableData";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <TableData></TableData>
     </main>
   );
 }
